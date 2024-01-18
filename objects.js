47091912ebbb09f8d7e67608f7e68478ad8f13b2
Manish Tiwari <77712914+manish2022-creator@users.noreply.github.com>
2024-01-18 19:10:52 +0000

    Objects basics

--- objects.js
+++ objects.js
@@ -1,1 +1,10 @@
 // singleton - ensures single instance in a class throughout lifetime of application
+const Jsuser = {
+    name : "Manish",
+    batch : "2023-27",
+    "branch" : "CSE",
+    emailId : "mtswarn666@gmail.com",
+    CGPA : 8,
+}
+console.log(Jsuser.name);
+console.log(Jsuser["branch"]);
