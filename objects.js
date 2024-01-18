f060699eee03a71acf68a699cb6d540ab54c31bf
Manish Tiwari <77712914+manish2022-creator@users.noreply.github.com>
2024-01-18 20:50:50 +0000

    18-01-2024

--- objects.js
+++ objects.js
@@ -1,10 +1,15 @@
 // singleton - ensures single instance in a class throughout lifetime of application
+const mySym = Symbol("keys")
 const Jsuser = {
     name : "Manish",
     batch : "2023-27",
     "branch" : "CSE",
     emailId : "mtswarn666@gmail.com",
     CGPA : 8,
+    mySym: "mykeys",
+
 }
 console.log(Jsuser.name);
 console.log(Jsuser["branch"]);
+console.log(mySym);
+console.log(typeof Jsuser.mySym);
